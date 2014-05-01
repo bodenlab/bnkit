a66d7a61523495cf34a652b4b4c2876eeb028fcd
m.boden <m.boden@uq.edu.au>
2014-05-01 02:11:13 +0000

    Fixed issues with MixtureDistrib, now flat, not recursive.

--- bnkit/src/bn/MixtureDistrib.java
+++ bnkit/src/bn/MixtureDistrib.java
@@ -17,13 +17,17 @@
 
 package bn;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * Mixture of probability distributions.
- * 
+ * Note that mixtures of mixtures is detected and avoided, leading to a flat mixture.
  * @author mikael
  */
 public class MixtureDistrib implements Distrib {
@@ -31,21 +35,23 @@ public class MixtureDistrib implements Distrib {
     final Map<Distrib, Double> mixture;
     private double density;
     Random rand = new Random();
-    
+
     public MixtureDistrib(Distrib d1, double weight1) {
         mixture = new HashMap<>();
-        mixture.put(d1, weight1);
-        density = weight1;
+        try {
+            MixtureDistrib packed = (MixtureDistrib) d1;
+            for (Map.Entry<Distrib, Double> entry : packed.mixture.entrySet()) {
+                double delta = entry.getValue() * weight1;
+                mixture.put(entry.getKey(), delta);
+                density += delta;
+            }
+        } catch (ClassCastException e) {
+            mixture.put(d1, weight1);
+            density = weight1;
+        }
     }
     
-    public MixtureDistrib(Distrib d1, double weight1, Distrib d2, double weight2) {
-        mixture = new HashMap<>();
-        mixture.put(d1, weight1);
-        mixture.put(d2, weight2);
-        density = weight1 + weight2;
-    }
-    
-    public double addDistrib(Distrib d2, double weight2) {
+    private double addDistribForced(Distrib d2, double weight2) {
         Double prev_weight = mixture.get(d2);
         if (prev_weight == null)
             mixture.put(d2, weight2);
@@ -53,6 +59,21 @@ public class MixtureDistrib implements Distrib {
             mixture.put(d2, prev_weight.doubleValue() + weight2);
         density += weight2;
         return density;
+    }
+
+    public double addDistrib(Distrib d2, double weight2) {
+        try {
+            MixtureDistrib packed = (MixtureDistrib) d2;
+            for (Map.Entry<Distrib, Double> entry : packed.mixture.entrySet())
+                addDistribForced(entry.getKey(), entry.getValue() * weight2);
+        } catch (ClassCastException e) {
+            addDistribForced(d2, weight2);
+        }
+        return density;
+    }
+    
+    public boolean hasDistrib(Distrib d2) {
+        return mixture.containsKey(d2);
     }
     
     @Override
@@ -88,4 +109,27 @@ public class MixtureDistrib implements Distrib {
         return sb.toString();
     }
     
+    public static void main(String[] args) {
+        GaussianDistrib gd1 = new GaussianDistrib(0, 1.0);
+        System.out.println("gd1 = " + gd1);
+        GaussianDistrib gd2 = new GaussianDistrib(1, 0.5);
+        System.out.println("gd2 = " + gd2);
+        GaussianDistrib gd3 = new GaussianDistrib(-2, 2.5);
+        System.out.println("gd3 = " + gd3);
+        MixtureDistrib md1 = new MixtureDistrib(gd1, 1.0);
+        md1.addDistrib(gd2, 2);
+        md1.addDistrib(gd2, 0.5);
+        System.out.println("md1 is gd1*1.0 + gd2*2.5 : \n" + md1);
+        MixtureDistrib md2 = new MixtureDistrib(md1, 1.0);
+        System.out.println("mds2 is md1*1.0 : \n" + md2);
+        md2.addDistrib(gd1, 0.5);
+        System.out.println("md2 += gd1*0.5 : \n" + md2);
+        md2.addDistrib(gd3, 2);
+        System.out.println("md2 += gd3*2.0 : \n" + md2);
+        md2.addDistrib(md1, 2.0);
+        System.out.println("md2 += md1*2.0 : \n" + md2);
+        md2.addDistrib(gd1, 1.5);
+        System.out.println("md2 += gd1*1.5 : \n" + md2);
+        System.out.println("density = " + md2.density);
+    }
 }
