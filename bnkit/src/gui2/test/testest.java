cc3a74e1431ad7cd70848b70004e94b820ffde17
m.boden <m.boden@uq.edu.au>
2014-06-24 03:10:52 +0000

    Tried to add Jun's GUI code, including a "test" sub-package. Had to change a few things to work. Previously NodeModel implemented BNode which caused issues so removed this. This in turn mean that some references to NodeModel in other code had to change too.

---
+++ bnkit/src/gui2/test/testest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui2.test;
+
+/**
+ *
+ * @author Jun
+ */
+public class testest {
+    private String stringy = "don't touch me!";
+    public void main (){
+        System.out.println("stringy");
+//        get() = "moo"; // required variable, found value
+        
+    }
+    
+    public String get(){
+        return stringy;
+    }
+    
+}
