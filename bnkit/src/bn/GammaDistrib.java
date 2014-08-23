a380fb68febf14390042d82bebe0a320660fe58b
m.boden <m.boden@uq.edu.au>
2014-08-23 00:58:47 +0000

    Adding stuff to DirichletDistrib, including MLE

--- bnkit/src/bn/GammaDistrib.java
+++ bnkit/src/bn/GammaDistrib.java
@@ -8,15 +8,21 @@ import java.util.*;
  * A Gamma distribution with shape parameter k and scale parameter 1/lambda.
  * Defined as f(x) = (lambda*e^(-lambda*x)*(lambda*x)^(k - 1)) / Gamma(k) where
  * Gamma(k) = integral from 0 to infinity of t^(k-1) * e^(-t) dt
- * Implementation is based on Bayesian Logic (BLOG) inference engine version 0.7 
- * (see copyright message below).
+ * Implementation is partly based on Bayesian Logic (BLOG) inference engine version 0.7 
+ * (see copyright message below). The digamma and trigamma implementations are 
+ * under an Apache Commons License as displayed below.
  */
 
 public class GammaDistrib implements Distrib, Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    
+    public static final double GAMMA = 0.577215664901532860606512090082;
+
+    // limits for switching algorithm in digamma
+    private static final double C_LIMIT = 49;
+    private static final double S_LIMIT = 1e-5;
+
     private double lambda;
     private double k;
     private Random rand = new Random();
@@ -113,7 +119,89 @@ public class GammaDistrib implements Distrib, Serializable {
         return (-tmp + Math.log(2.5066282746310005 * ser / x));
     }
 
+    /*
+    The following two methods: digamma and trigamma are ...
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    
+    The code has been modified as a result of the incorporation into bnkit.
+    */
+    /**
+     * <p>Computes the digamma function of x.</p>
+     *
+     * <p>This is an independently written implementation of the algorithm described in
+     * Jose Bernardo, Algorithm AS 103: Psi (Digamma) Function, Applied Statistics, 1976.</p>
+     *
+     * <p>Some of the constants have been changed to increase accuracy at the moderate expense
+     * of run-time.  The result should be accurate to within 10^-8 absolute tolerance for
+     * x >= 10^-5 and within 10^-8 relative tolerance for x > 0.</p>
+     *
+     * <p>Performance for large negative values of x will be quite expensive (proportional to
+     * |x|).  Accuracy for negative values of x should be about 10^-8 absolute for results
+     * less than 10^5 and 10^-8 relative for results larger than that.</p>
+     *
+     * @param x Argument.
+     * @return digamma(x) to within 10-8 relative or absolute error whichever is smaller.
+     * @see <a href="http://en.wikipedia.org/wiki/Digamma_function">Digamma</a>
+     * @see <a href="http://www.uv.es/~bernardo/1976AppStatist.pdf">Bernardo&apos;s original article </a>
+     * @since 2.0
+     */
+    public static double digamma(double x) {
+        if (x > 0 && x <= S_LIMIT) {
+            // use method 5 from Bernardo AS103
+            // accurate to O(x)
+            return -GAMMA - 1 / x;
+        }
+        if (x >= C_LIMIT) {
+            // use method 4 (accurate to O(1/x^8)
+            double inv = 1 / (x * x);
+            //            1       1        1         1
+            // log(x) -  --- - ------ + ------- - -------
+            //           2 x   12 x^2   120 x^4   252 x^6
+            return Math.log(x) - 0.5 / x - inv * ((1.0 / 12) + inv * (1.0 / 120 - inv / 252));
+        }
+        return digamma(x + 1) - 1 / x;
+    }
+
+    /**
+     * Computes the trigamma function of x.
+     * This function is derived by taking the derivative of the implementation
+     * of digamma.
+     *
+     * @param x Argument.
+     * @return trigamma(x) to within 10-8 relative or absolute error whichever is smaller
+     * @see <a href="http://en.wikipedia.org/wiki/Trigamma_function">Trigamma</a>
+     * @see Gamma#digamma(double)
+     * @since 2.0
+     */
+    public static double trigamma(double x) {
+        if (x > 0 && x <= S_LIMIT) {
+            return 1 / (x * x);
+        }
+        if (x >= C_LIMIT) {
+            double inv = 1 / (x * x);
+            //  1    1      1       1       1
+            //  - + ---- + ---- - ----- + -----
+            //  x      2      3       5       7
+            //      2 x    6 x    30 x    42 x
+            return 1 / x + inv / 2 + inv / x * (1.0 / 6 - inv * (1.0 / 30 + inv / 42));
+        }
+        return trigamma(x + 1) + 1 / (x * x);
+    }
+
 }
+
+
 /*
  * Copyright (c) 2005, Regents of the University of California
  * All rights reserved.
