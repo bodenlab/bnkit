de2c304d7c3eb62548ddbb755c42b59145d6fb37
m.boden@uq.edu.au <m.boden@uq.edu.au@ba354d33-888f-1958-9764-d890e4f7a03d>
2014-09-17 23:23:17 +0000

    Continuous time Markov chain nodes incorporated. Just initial classes and not yet full functionality.

---
+++ trunk/bnkit/src/bn/ctmc/SubstModel.java
@@ -0,0 +1,170 @@
+/*
+ bnkit -- software for building and using Bayesian networks
+ Copyright (C) 2014  M. Boden et al.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bn.ctmc;
+
+import bn.Enumerable;
+import bn.math.Matrix.*;
+import bn.ctmc.matrix.*;
+
+/**
+ *
+ * @author mikael
+ */
+public class SubstModel {
+    
+    final double[][] R;
+    final Exp Rexp;
+    final Enumerable alpha;
+
+    /**
+     * 
+     * @param F stationary base frequencies
+     * @param Q Q matrix (general reversible model)
+     * @param alphabet the values that substitutable variables can take, listed strictly in the order of the array and matrix
+     */
+    public SubstModel(double[] F, double[][] Q, Enumerable alphabet) {
+        if (Q.length != F.length)
+            throw new IllegalArgumentException("Invalid size of either Q or F");
+        if (alphabet.size() != F.length)
+            throw new IllegalArgumentException("Invalid size of alphabet");
+        R = new double[Q.length][Q.length];
+        for (int i = 0; i < Q.length; i ++)  {
+            if (Q[i].length != F.length)
+                throw new IllegalArgumentException("Q must be a square matrix");
+            for (int j = i + 1; j < Q[i].length; j ++) {
+                double q = Q[i][j];
+                R[i][j] = q*F[j];
+                R[j][i] = q*F[i];
+            }
+        }
+        this.alpha = alphabet;
+        SubstModel.makeValid(R);
+        SubstModel.normalize(F, R);
+        Rexp = new Exp(R);
+    }
+   
+    /** 
+     * Make it a valid rate matrix (make sum of rows = 0) "in place"
+     * @param R the potentially invalid R, to be modified in place
+     */
+    private static void makeValid(double[][] R) {
+        int dim = R.length;
+        for (int i = 0; i < dim; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < dim; j++) {
+                if (i != j)
+                    sum += R[i][j];
+            }
+            R[i][i] = -sum;
+        }
+    }
+
+    /**
+     * Normalize rate matrix "in place" to one expected substitution per unit time.
+     * @param F stationary base frequencies
+     * @param R the potentially un-normalised R-matrix, to be modified in place
+     */
+    private static void normalize(double[] F, double[][] R) {
+        int dim = R.length;
+        double sum = 0.0;
+        for (int i = 0; i < dim; i++) {
+            sum += -R[i][i]*F[i];
+        }
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++)
+                R[i][j] = R[i][j]/sum;
+        }
+    }
+
+    private double[][] probs = null;
+    private double time = 0.0;
+    // private boolean updateRequired = true;
+    
+    public double getProb(Object X, Object Y, double time) {
+        if (this.time != time || probs == null) // only re-compute matrix if time has changed
+            probs = getProbs(time);
+        
+        int index_X = alpha.getIndex(X);
+        int index_Y = alpha.getIndex(Y);
+        return probs[index_Y][index_X];
+    }
+    
+    /**
+     * Compute the transition probabilities for an expected distance
+     * using the pre-specified rate matrix
+     *
+     * @param time expected distance
+     * @return the conditional probabilities of a symbol at time t+time GIVEN a symbol at time t  [row: X(t)][col: X(t+time)]
+     */
+    private final double[][] getProbs(double time) {
+        this.time = time;
+        int i, j, k;
+        double temp;
+        double[] eval = Rexp.getEigval();
+        double[][] ievec = Rexp.getInvEigvec();
+        double[][] evec = Rexp.getEigvec();
+        double[][] iexp = new double[R.length][R.length];
+        double[][] prob = new double[R.length][R.length];
+        for (k = 0; k < R.length; k++) {
+            temp = Math.exp(time * eval[k]);
+            for (j = 0; j < R.length; j++) {
+                iexp[k][j] = ievec[k][j] * temp;
+            }
+        }
+        for (i = 0; i < R.length; i++) {
+            for (j = 0; j < R.length; j++) {
+                temp = 0.0;
+                for (k = 0; k < R.length; k++) {
+                    temp += evec[i][k] * iexp[k][j];
+                }
+                prob[i][j] = Math.abs(temp);
+            }
+        }
+        return prob;
+    }
+    
+    
+    public static void main(String[] argv) {
+        SubstModel sm_wag = new SubstModel(WAG.F, WAG.Q, Enumerable.aacid_alt);
+        SubstModel sm_lg = new SubstModel(LG.F, LG.Q, Enumerable.aacid_alt);
+        SubstModel sm_jtt = new SubstModel(JTT.F, JTT.Q, Enumerable.aacid_alt);
+        SubstModel sm_dh = new SubstModel(Dayhoff.F, Dayhoff.Q, Enumerable.aacid_alt);
+
+        System.out.println("R (WAG)");
+        bn.math.Matrix.print(sm_wag.R);
+        System.out.println("\nR (LG)");
+        bn.math.Matrix.print(sm_lg.R);
+
+        double time = 1.0;
+        System.out.println("\n\nTransition probabilities of R (WAG) @ time = " + time);
+        double[][] prob = sm_wag.getProbs(time);
+        bn.math.Matrix.print(prob);
+        System.out.println("\nTransition probabilities of R (LG) @ time = " + time);
+        prob = sm_lg.getProbs(time);
+        bn.math.Matrix.print(prob);
+        System.out.println("\nTransition probabilities of R (JTT) @ time = " + time);
+        prob = sm_jtt.getProbs(time);
+        bn.math.Matrix.print(prob);
+        System.out.println("\nTransition probabilities of R (Dayhoff) @ time = " + time);
+        prob = sm_dh.getProbs(time);
+        bn.math.Matrix.print(prob);
+        double p = sm_dh.getProb('K', 'R', time);
+        System.out.println("P(K|R) = " + p);
+    }
+}
