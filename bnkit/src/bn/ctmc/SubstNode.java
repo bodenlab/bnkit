dc2f2e262f921d7e4fc53458353a476b2c641edb
m.boden <m.boden@uq.edu.au>
2014-10-22 03:36:05 +0000

    minor

--- bnkit/src/bn/ctmc/SubstNode.java
+++ bnkit/src/bn/ctmc/SubstNode.java
@@ -1,29 +1,29 @@
 /*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    bnkit -- software for building and using Bayesian networks
+    Copyright (C) 2014  M. Boden et al.
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package bn.ctmc;
 
 import bn.*;
 import bn.alg.CGTable;
-import bn.alg.CGVarElim;
 import bn.alg.Query;
 import bn.alg.VarElim;
 import bn.ctmc.matrix.JTT;
 import bn.factor.AbstractFactor;
 import bn.factor.DenseFactor;
-import bn.factor.Factorize;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
