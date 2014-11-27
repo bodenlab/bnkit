7e41c9a9dbbb0eeaa787a44b91d8ffff2df8ac99
GaryYufei <GaryYufei@gmail.com>
2014-11-27 03:09:12 +0000

    finish naive Bayes..but bad prediction result

--- bnkit/src/bn/example/SimpleExample.java
+++ bnkit/src/bn/example/SimpleExample.java
@@ -105,7 +105,7 @@ public class SimpleExample {
             CGTable cgt = (CGTable)ai.infer(qq);
             cgt.display();
 
-            BNBuf.save(bn, "data/bn_simple.xml");
+            //BNBuf.save(bn, "data/bn_simple.xml");
 		
 	}
 	
