0f092b4c9baf5a353ab1f3f9326faf77890b1ee0
GaryYufei@gmail.com <GaryYufei@gmail.com@ba354d33-888f-1958-9764-d890e4f7a03d>
2015-01-03 06:13:25 +0000

    add some implementation for mixDirichlet class

--- trunk/bnkit/src/bn/prob/MixDirichletDistrib.java
+++ trunk/bnkit/src/bn/prob/MixDirichletDistrib.java
@@ -2,20 +2,58 @@ package bn.prob;
 
 import java.io.Serializable;
 
+import dat.Enumerable;
+
 import bn.Distrib;
+import bn.prior.DirichletDistribPrior;
 
-public class MixDirichletDistrib implements Serializable, Distrib {
+/**
+ * Mixture Dirichlet distribution is weighted sum of different Dirichlet distribution
+ * MDir = w1 * Dir1 + w2 * Dir2 + ... 
+ * This class includes learning parameters from data
+ * @author wangyufei
+ *
+ */
 
-	@Override
-	public double get(Object value) {
-		// TODO Auto-generated method stub
-		return 0;
+public class MixDirichletDistrib  extends MixtureDistrib implements Serializable{
+
+	/**
+	 * construct a mixture Dirichlet model from a single component
+	 * @param d1
+	 * @param weight1
+	 */
+	public MixDirichletDistrib(DirichletDistrib d1, double weight1) {
+		super(d1, weight1);
+	}
+	
+	/**
+	 * given the domain of Dirichlet distribution
+	 * build an empty Mixture model
+	 * @param domain
+	 * @param ComponentNum
+	 */
+	public MixDirichletDistrib(Enumerable domain, int ComponentNum) {
+		super(new DirichletDistrib(domain), 1.0);
+		for(int i = 0; i < ComponentNum - 1; i++) {
+			super.addDistrib(new DirichletDistrib(domain), 1.0);
+		}
+	}
+	
+	/**
+	 * add either a Dirichlet distribution or mixDirichlet distribution
+	 */
+	public double addDistrib(Distrib d2, double weight2) {
+		if(d2 instanceof DirichletDistrib || d2 instanceof MixDirichletDistrib) {
+			return super.addDistrib(d2, weight2);
+		}
+		throw new RuntimeException("only accept DirichletDistrib or MixDirichletDistrib");
+		
+	}
+	
+	public void learnParameters(Object[] data, Double[] weight) {
+		// TODO
 	}
 
-	@Override
-	public Object sample() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
