4976b058c3aa87747fcb65c9555fec812c9163d3
GaryYufei <GaryYufei@gmail.com>
2015-02-03 03:13:27 +0000

    add mixture Dirichlet distribution parameter manager

---
+++ bnkit/src/bn/prior/MixDirichletManager.java
@@ -0,0 +1,31 @@
+package bn.prior;
+
+/**
+ * This is the manager for saving and loading the parameters of Mixture Dirichlet model
+ * @author wangyufei
+ *
+ */
+public class MixDirichletManager {
+
+	/**
+	 * save the parameters of mixturePrior 
+	 * @param mixPrior
+	 */
+	public void save(MixDirichletPrior mixPrior) {
+		
+	}
+	
+	public MixDirichletPrior load() {
+		return null;
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
