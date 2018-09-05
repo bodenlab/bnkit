b86ee990ad8f584038651393658901b82ff6014b
gabefoley <gabriel.foley@uqconnect.edu.au>
2018-09-05 11:37:19 +1000

    Commented out test with missing file in ParsimonyTests

--- bnkit/src/test/ParsimonyTests.java
+++ bnkit/src/test/ParsimonyTests.java
@@ -28,12 +28,12 @@ public class ParsimonyTests {
             trees = new PhyloTree[] { // MB: also successfully tried a 150-seq tree with alignment (not in test/resources)
                     PhyloTree.loadNewick("src/test/resources/large.nwk"),
                     PhyloTree.loadNewick("src/test/resources/default.nwk"),
-                    PhyloTree.loadNewick("src/test/resources/edge1.nwk"),
+//                    PhyloTree.loadNewick("src/test/resources/edge1.nwk"),
             };
             alns = new EnumSeq.Alignment[] {
                     new EnumSeq.Alignment(EnumSeq.Gappy.loadClustal("src/test/resources/large.aln", Enumerable.aacid)),
                     new EnumSeq.Alignment(EnumSeq.Gappy.loadClustal("src/test/resources/default.aln", Enumerable.aacid)),
-                    new EnumSeq.Alignment(EnumSeq.Gappy.loadClustal("src/test/resources/edge2.aln", Enumerable.aacid)),
+//                    new EnumSeq.Alignment(EnumSeq.Gappy.loadClustal("src/test/resources/edge2.aln", Enumerable.aacid)),
             };
             for (int i = 0; i < trees.length; i ++)
                 trees[i].setAlignment(alns[i]);
