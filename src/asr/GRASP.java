8365f23dc382680fe0821ca80860d5e1c8b66c3e
Sebastian <s.porras@uq.edu.au>
2026-01-30 09:38:31 +1000

    update version date

--- src/asr/GRASP.java
+++ src/asr/GRASP.java
@@ -28,7 +28,7 @@ import asr.IndelDist.RATE_CATEGORY;
  */
 public class GRASP {
 
-    public static String VERSION = "17-Dec-2025";
+    public static String VERSION = "30-Jan-2026";
 
     public static boolean VERBOSE  = false;
     public static boolean TIME     = false;
